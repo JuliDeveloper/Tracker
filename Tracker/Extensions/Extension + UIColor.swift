ea071aa7e5bd339b1edc5ce8ce8d0c3aa4aea51d
JuliaRomanenko <rja-97@yandex.ru>
2023-03-28 17:33:56 +0300

    SPRINT-14: Добавила кастомные цвета и реализовала extension для UIColor; Добавила AppIcon

---
+++ Tracker/Extensions/Extension + UIColor.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+extension UIColor {
+    static var ypWhiteDay: UIColor { UIColor(named: "ypWhiteDay") ?? UIColor() }
+    static var ypBlackDay: UIColor { UIColor(named: "ypBlackDay") ?? UIColor() }
+    static var ypBackgroundDay: UIColor { UIColor(named: "ypBackgroundDay") ?? UIColor() }
+    static var ypWhiteNight: UIColor { UIColor(named: "ypWhiteNight") ?? UIColor() }
+    static var ypBlackNight: UIColor { UIColor(named: "ypBlackNight") ?? UIColor() }
+    static var ypBackgroundNight: UIColor { UIColor(named: "ypBackgroundNight") ?? UIColor() }
+    static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor() }
+    static var ypBlue: UIColor { UIColor(named: "ypBlue") ?? UIColor() }
+    static var ypGray: UIColor { UIColor(named: "ypGray") ?? UIColor() }
+    static var ypLightGray: UIColor { UIColor(named: "ypLightGray") ?? UIColor() }
+    static var ypColorSection1: UIColor { UIColor(named: "ypColorSection1") ?? UIColor() }
+    static var ypColorSection2: UIColor { UIColor(named: "ypColorSection2") ?? UIColor() }
+    static var ypColorSection3: UIColor { UIColor(named: "ypColorSection3") ?? UIColor() }
+    static var ypColorSection4: UIColor { UIColor(named: "ypColorSection4") ?? UIColor() }
+    static var ypColorSection5: UIColor { UIColor(named: "ypColorSection5") ?? UIColor() }
+    static var ypColorSection6: UIColor { UIColor(named: "ypColorSection6") ?? UIColor() }
+    static var ypColorSection7: UIColor { UIColor(named: "ypColorSection7") ?? UIColor() }
+    static var ypColorSection8: UIColor { UIColor(named: "ypColorSection8") ?? UIColor() }
+    static var ypColorSection9: UIColor { UIColor(named: "ypColorSection9") ?? UIColor() }
+    static var ypColorSection10: UIColor { UIColor(named: "ypColorSection10") ?? UIColor() }
+    static var ypColorSection11: UIColor { UIColor(named: "ypColorSection11") ?? UIColor() }
+    static var ypColorSection12: UIColor { UIColor(named: "ypColorSection12") ?? UIColor() }
+    static var ypColorSection13: UIColor { UIColor(named: "ypColorSection13") ?? UIColor() }
+    static var ypColorSection14: UIColor { UIColor(named: "ypColorSection14") ?? UIColor() }
+    static var ypColorSection15: UIColor { UIColor(named: "ypColorSection15") ?? UIColor() }
+    static var ypColorSection16: UIColor { UIColor(named: "ypColorSection16") ?? UIColor() }
+    static var ypColorSection17: UIColor { UIColor(named: "ypColorSection17") ?? UIColor() }
+    static var ypColorSection18: UIColor { UIColor(named: "ypColorSection18") ?? UIColor() }
+}
