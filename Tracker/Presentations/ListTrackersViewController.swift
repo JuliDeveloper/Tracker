7094b1b608d5f4246ec08833e05874c79742ac9f
JuliaRomanenko <rja-97@yandex.ru>
2023-03-29 12:53:59 +0300

    SPRINT-14: Добавила items для UITabBar: ListTrackersVC и StatisticsVC

---
+++ Tracker/Presentations/ListTrackersViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class ListTrackersViewController: UIViewController {
+    //MARK: - Properties
+
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        configureView()
+        
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .ypWhite
+    }
+    
+    //MARK: - Helpers
+
+}
