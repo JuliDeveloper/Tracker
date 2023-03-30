7956c6b8919f1c91dfae65fc0dabfaacd60448cb
JuliaRomanenko <rja-97@yandex.ru>
2023-03-30 16:17:32 +0300

    SPRINT-14: Добавила кастомную кнопку в проект

---
+++ Tracker/Presentations/Views/CustomButton.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+final class CustomButton: UIButton {
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        backgroundColor = .ypBlack
+        titleLabel?.font = UIFont.ypFontMedium16
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: 60).isActive = true
+        layer.cornerRadius = Constants.bigRadius
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
