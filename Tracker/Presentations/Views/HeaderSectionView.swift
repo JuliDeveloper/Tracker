985f90f4a3d8482b0b5b0c9eda39f984c2fce4af
JuliaRomanenko <rja-97@yandex.ru>
2023-03-30 15:40:58 +0300

    SPRINT-14: Настроила кастомный header для collectionView

---
+++ Tracker/Presentations/Views/HeaderSectionView.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+final class HeaderSectionView: UICollectionReusableView {
+    
+    //MARK: - Properties
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .ypBlack
+        label.font = UIFont.ypFontBold19
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //MARK: - Helpers
+    func configureHeader(title: String) {
+        addSubview(titleLabel)
+        titleLabel.text = title
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(
+                equalTo: topAnchor, constant: 24
+            ),
+            titleLabel.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 28
+            ),
+            titleLabel.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -28
+            )
+        ])
+    }
+}
