65d30eababc57f15caacfe0d1ba054759f0024f5
JuliaRomanenko <rja-97@yandex.ru>
2023-04-02 21:13:51 +0300

    SPRINT-14: Создала модели данных

---
+++ Tracker/Models/Schedule.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct Schedule {
+    let weekDays: [WeekDay: Bool]
+}
+
+enum WeekDay: String {
+    case monday = "Понедельник"
+    case tuesday = "Вторник"
+    case wednesday = "Среда"
+    case thursday = "Четверг"
+    case friday = "Пятница"
+    case saturday = "Суббота"
+    case sunday = "Воскресенье"
+}
