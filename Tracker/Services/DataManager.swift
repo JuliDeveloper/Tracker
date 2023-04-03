cbbade09de6ebe251ea686ba793913df4e7aef6b
JuliaRomanenko <rja-97@yandex.ru>
2023-04-03 17:20:17 +0300

    SPRINT-14: Создала временный dataManager

--- Tracker/Services/DataManager.swift
+++ Tracker/Services/DataManager.swift
@@ -3,28 +3,28 @@ import Foundation
 class DataManager {
     static let shared = DataManager()
     
+    var category = TrackerCategory(
+        title: "Радостные мелочи",
+        trackers: [
+            Tracker(
+                id: UUID(),
+                title: "Поливать растения",
+                color: .ypColorSection5,
+                emoji: "❤️",
+                schedule: nil
+            ),
+            Tracker(
+                id: UUID(),
+                title: "Свидание в апреле",
+                color: .ypColorSection14,
+                emoji: "🌺",
+                schedule: nil
+            )
+        ]
+    )
+    
     func getCategories() -> [TrackerCategory] {
         var dataCategories: [TrackerCategory] = []
-        
-        let category = TrackerCategory(
-            title: "Радостные мелочи",
-            trackers: [
-                Tracker(
-                    id: UUID(),
-                    title: "Поливать растения",
-                    color: .ypColorSection5,
-                    emoji: "❤️",
-                    schedule: nil
-                ),
-                Tracker(
-                    id: UUID(),
-                    title: "Свидание в апреле",
-                    color: .ypColorSection14,
-                    emoji: "🌺",
-                    schedule: nil
-                )
-            ]
-        )
         
         dataCategories.append(category)
         
