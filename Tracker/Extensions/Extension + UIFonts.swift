60fd2291436c6370a4906a59deb2a720e9e08d00
JuliaRomanenko <rja-97@yandex.ru>
2023-04-16 22:45:39 +0300

    SPRINT-15: Заменила шрифт на системный, тк поменяли в макете

--- Tracker/Extensions/Extension + UIFonts.swift
+++ Tracker/Extensions/Extension + UIFonts.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 extension UIFont {
-    static var ypFontBold34 = UIFont(name: "YSDisplay-Bold", size: 34) ?? UIFont()
-    static var ypFontBold32 = UIFont(name: "YSDisplay-Bold", size: 32) ?? UIFont()
-    static var ypFontBold19 = UIFont(name: "YSDisplay-Bold", size: 19) ?? UIFont()
-    static var ypFontMedium17 = UIFont(name: "YSDisplay-Medium", size: 17) ?? UIFont()
-    static var ypFontMedium16 = UIFont(name: "YSDisplay-Medium", size: 16) ?? UIFont()
-    static var ypFontMedium12 = UIFont(name: "YSDisplay-Medium", size: 12) ?? UIFont()
-    static var ypFontMedium10 = UIFont(name: "YSDisplay-Medium", size: 10) ?? UIFont()
+    static var ypFontBold34 = UIFont.systemFont(ofSize: 34, weight: .bold)
+    static var ypFontBold32 = UIFont.systemFont(ofSize: 32, weight: .bold)
+    static var ypFontBold19 = UIFont.systemFont(ofSize: 19, weight: .bold)
+    static var ypFontMedium17 = UIFont.systemFont(ofSize: 17, weight: .regular)
+    static var ypFontMedium16 = UIFont.systemFont(ofSize: 16, weight: .medium)
+    static var ypFontMedium12 = UIFont.systemFont(ofSize: 12, weight: .medium)
+    static var ypFontMedium10 = UIFont.systemFont(ofSize: 10, weight: .medium)
 }
