9d4258968a104b6180b8bc6be56c6af898013776
JuliaRomanenko <rja-97@yandex.ru>
2023-04-27 18:50:40 +0300

    SPRINT-15: 1. Добавила TrackerStorage; 2. Добавила класс, который конвертирует цвет из UIColor в HEX и наоборот

---
+++ Tracker/Services/TrackerStore.swift
@@ -0,0 +1,161 @@
+import UIKit
+import CoreData
+
+enum TrackerStoreError: Error {
+    case decodingErrorInvalidId
+    case decodingErrorInvalidTitle
+    case decodingErrorInvalidColor
+    case decodingErrorInvalidEmoji
+    case decodingErrorInvalidSchedule
+}
+
+struct TrackerStoreUpdate {
+   let insertedIndexes: IndexSet
+}
+
+final class TrackerStore: NSObject {
+    
+    //MARK: - Properties
+    private let context: NSManagedObjectContext
+    private let uiColorMarshalling = UIColorMarshalling()
+    private let trackerCategoryStore = TrackerCategoryStore()
+    
+    private var insertedIndexes: IndexSet?
+    
+    private lazy var fetchedResultsController: NSFetchedResultsController<TrackerCoreData> = {
+        let fetchRequest = NSFetchRequest<TrackerCoreData>(entityName: "TrackerCoreData")
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(keyPath: \TrackerCoreData.category?.title, ascending: true),
+            NSSortDescriptor(keyPath: \TrackerCoreData.createdAt, ascending: true)
+        ]
+        let fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: context,
+            sectionNameKeyPath: "category",
+            cacheName: nil
+        )
+        fetchedResultsController.delegate = self
+        try? fetchedResultsController.performFetch()
+        return fetchedResultsController
+    }()
+        
+    // MARK: - Lifecycle
+    convenience override init() {
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        self.init(context: context)
+    }
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+        super.init()
+    }
+    
+    //MARK: - Helpers
+    func getTracker(from trackerCoreData: TrackerCoreData) throws -> Tracker {
+        guard let id = trackerCoreData.trackerId else {
+            throw TrackerStoreError.decodingErrorInvalidId
+        }
+
+        guard let title = trackerCoreData.title else {
+            throw TrackerStoreError.decodingErrorInvalidTitle
+        }
+
+        guard let colorHex = trackerCoreData.colorHex else {
+            throw TrackerStoreError.decodingErrorInvalidColor
+        }
+
+        guard let emoji = trackerCoreData.emoji else {
+            throw TrackerStoreError.decodingErrorInvalidEmoji
+        }
+        
+        guard let countRecords = trackerCoreData.records?.count else {
+            throw TrackerStoreError.decodingErrorInvalidEmoji
+        }
+
+        //разобраться с расписанием !!!
+//        guard let scheduleSet = trackerCoreData.schedule else {
+//            throw TrackerStoreError.decodingErrorInvalidSchedule
+//        }
+
+        let color = uiColorMarshalling.fromHexString(hex: colorHex)
+        //let schedule = Array(scheduleSet)
+
+        return Tracker(
+            id: id,
+            title: title,
+            color: color,
+            emoji: emoji,
+            schedule: nil,
+            countRecords: countRecords
+        )
+    }
+    
+    func getTrackerCoreData(by id: UUID) throws -> TrackerCoreData? {
+        fetchedResultsController.fetchRequest.predicate = NSPredicate(
+            format: "%K == %@",
+            #keyPath(TrackerCoreData.trackerId), id as CVarArg
+        )
+        
+        do {
+            try fetchedResultsController.performFetch()
+            return fetchedResultsController.fetchedObjects?.first
+        } catch {
+            throw error
+        }
+    }
+}
+
+extension TrackerStore: TrackerStoreProtocol {
+    var countTrackers: Int {
+        fetchedResultsController.fetchedObjects?.count ?? 0
+    }
+    
+    var numberOfSections: Int {
+        fetchedResultsController.sections?.count ?? 0
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func headerTitleSection(_ section: Int) -> String? {
+        guard let trackerCoreData = fetchedResultsController.sections?[section].objects?.first as? TrackerCoreData else { return "" }
+        return trackerCoreData.category?.title
+    }
+    
+    func getTracker(at indexPath: IndexPath) -> Tracker? {
+        let trackerCoreData = fetchedResultsController.object(at: indexPath)
+        
+        do {
+            let tracker = try getTracker(from: trackerCoreData)
+            return tracker
+        } catch {
+            return nil
+        }
+    }
+    
+    func addNewTracker(from tracker: Tracker, and category: TrackerCategory) throws {
+        let categoryCoreData = try trackerCategoryStore.categoryCoreData(with: category.title)
+        
+        let trackerCoreData = TrackerCoreData(context: context)
+        trackerCoreData.trackerId = tracker.id
+        trackerCoreData.title = tracker.title
+        trackerCoreData.emoji = tracker.emoji
+        trackerCoreData.colorHex = uiColorMarshalling.toHexString(color: tracker.color)
+        trackerCoreData.schedule = nil
+        trackerCoreData.createdAt = Date()
+        trackerCoreData.category = categoryCoreData
+
+        try context.save()
+    }
+}
+
+extension TrackerStore: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        insertedIndexes = IndexSet()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.insertedIndexes = nil
+    }
+}
