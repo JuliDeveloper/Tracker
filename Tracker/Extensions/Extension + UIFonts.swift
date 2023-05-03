7b117909ccabd6e5c344a0b08662417152ed87a2
JuliaRomanenko <rja-97@yandex.ru>
2023-05-03 16:25:53 +0300

    SPRINT-15: Сделала свойства иммутабельными

--- Tracker/Extensions/Extension + UIFonts.swift
+++ Tracker/Extensions/Extension + UIFonts.swift
@@ -1,11 +1,11 @@
 import UIKit
 
 extension UIFont {
-    static var ypFontBold34 = UIFont.systemFont(ofSize: 34, weight: .bold)
-    static var ypFontBold32 = UIFont.systemFont(ofSize: 32, weight: .bold)
-    static var ypFontBold19 = UIFont.systemFont(ofSize: 19, weight: .bold)
-    static var ypFontMedium17 = UIFont.systemFont(ofSize: 17, weight: .regular)
-    static var ypFontMedium16 = UIFont.systemFont(ofSize: 16, weight: .medium)
-    static var ypFontMedium12 = UIFont.systemFont(ofSize: 12, weight: .medium)
-    static var ypFontMedium10 = UIFont.systemFont(ofSize: 10, weight: .medium)
+    static let ypFontBold34 = UIFont.systemFont(ofSize: 34, weight: .bold)
+    static let ypFontBold32 = UIFont.systemFont(ofSize: 32, weight: .bold)
+    static let ypFontBold19 = UIFont.systemFont(ofSize: 19, weight: .bold)
+    static let ypFontMedium17 = UIFont.systemFont(ofSize: 17, weight: .regular)
+    static let ypFontMedium16 = UIFont.systemFont(ofSize: 16, weight: .medium)
+    static let ypFontMedium12 = UIFont.systemFont(ofSize: 12, weight: .medium)
+    static let ypFontMedium10 = UIFont.systemFont(ofSize: 10, weight: .medium)
 }
