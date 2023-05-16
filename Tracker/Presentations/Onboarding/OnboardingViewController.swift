9716ec77676f7fa5bb39355415dbc67e62e7e8d4
JuliaRomanenko <rja-97@yandex.ru>
2023-05-16 17:08:54 +0300

    SPRINT-16: Поправила код для OnboardingViewController и CustomViewController

--- Tracker/Presentations/Onboarding/OnboardingViewController.swift
+++ Tracker/Presentations/Onboarding/OnboardingViewController.swift
@@ -4,55 +4,29 @@ final class OnboardingViewController: UIPageViewController {
     
     //MARK: - Properties
     private lazy var pages: [UIViewController] = {
-        let bluePage = CustomViewController(imageTitle: "pageBlue")
-        let redPage = CustomViewController(imageTitle: "pageRed")
+        let bluePage = CustomViewController(
+            imageTitle: "pageBlue",
+            textLabel: "Отслеживайте только то, что хотите"
+        )
+        let redPage = CustomViewController(
+            imageTitle: "pageRed",
+            textLabel: "Даже если это не литры воды и йога"
+        )
         
         return [bluePage, redPage]
-    }()
-    
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.ypFontBold32
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let onboardingStack: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .vertical
-        stack.distribution = .fill
-        stack.spacing = 24
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        return stack
     }()
     
     private lazy var pageControl: UIPageControl = {
         let pageControl = UIPageControl()
         pageControl.numberOfPages = pages.count
         pageControl.currentPage = 0
-        
         pageControl.currentPageIndicatorTintColor = .ypDefaultBlack
         pageControl.pageIndicatorTintColor = .ypDefaultBlack.withAlphaComponent(0.3)
-        
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
         return pageControl
     }()
     
-    private let openListTrackerButton = CustomButton(title: "Вот это технологии!")
-    
     //MARK: - Lifecycle
-    init() {
-        super.init(transitionStyle: .scroll,
-                   navigationOrientation: .horizontal,
-                   options: nil
-        )
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -60,57 +34,23 @@ final class OnboardingViewController: UIPageViewController {
         delegate = self
         
         if let firstVC = pages.first {
-            titleLabel.text = "Отслеживайте только то, что хотите"
             setViewControllers([firstVC], direction: .forward, animated: true)
         }
         
-        addElements()
+        view.addSubview(pageControl)
         setConstraints()
-        
-        openListTrackerButton.addTarget(
-            self,
-            action: #selector(openTrackers),
-            for: .touchUpInside
-        )
     }
     
     //MARK: - Helpers
-    private func addElements() {
-        view.addSubview(titleLabel)
-        view.addSubview(onboardingStack)
-        
-        onboardingStack.addArrangedSubview(pageControl)
-        onboardingStack.addArrangedSubview(openListTrackerButton)
-    }
-    
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            onboardingStack.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: 20
+            pageControl.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
             ),
-            onboardingStack.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor, constant: -20
-            ),
-            onboardingStack.bottomAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -71
-            ),
-            
-            titleLabel.leadingAnchor.constraint(
-                equalTo: view.leadingAnchor, constant: 16
-            ),
-            titleLabel.trailingAnchor.constraint(
-                equalTo: view.trailingAnchor, constant: -16
-            ),
-            titleLabel.bottomAnchor.constraint(
-                equalTo: onboardingStack.topAnchor, constant: -130
+            pageControl.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -155
             )
         ])
-    }
-    
-    @objc private func openTrackers() {
-        let tabBarVC = TabBarController()
-        tabBarVC.modalPresentationStyle = .fullScreen
-        present(tabBarVC, animated: true)
     }
 }
 
@@ -150,15 +90,6 @@ extension OnboardingViewController: UIPageViewControllerDelegate {
         if let currentVC = pageViewController.viewControllers?.first,
            let currentIndex = pages.firstIndex(of: currentVC) {
             pageControl.currentPage = currentIndex
-            
-            switch currentIndex {
-            case 0:
-                titleLabel.text = "Отслеживайте только то, что хотите"
-            case 1:
-                titleLabel.text = "Даже если это не литры воды и йога"
-            default:
-                break
-            }
         }
     }
 }
--- Tracker/Presentations/Views/CustomViewController.swift
+++ Tracker/Presentations/Views/CustomViewController.swift
@@ -9,10 +9,31 @@ final class CustomViewController: UIViewController {
         return imageView
     }()
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.ypFontBold32
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var openListTrackerButton: UIButton = {
+        let button = CustomButton(title: "Вот это технологии!")
+        button.addTarget(
+            self,
+            action: #selector(openTrackers),
+            for: .touchUpInside
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     //MARK: - Lifecycle
-    init(imageTitle: String) {
+    init(imageTitle: String, textLabel: String) {
         super.init(nibName: nil, bundle: nil)
         imageView.image = UIImage(named: imageTitle)
+        titleLabel.text = textLabel
     }
     
     required init?(coder: NSCoder) {
@@ -21,13 +42,36 @@ final class CustomViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addElements()
+        setConstraint()
+    }
+    
+    private func addElements() {
         view.addSubview(imageView)
-        
+        view.addSubview(titleLabel)
+        view.addSubview(openListTrackerButton)
+    }
+    
+    private func setConstraint() {
         NSLayoutConstraint.activate([
             imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             imageView.topAnchor.constraint(equalTo: view.topAnchor),
             imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            openListTrackerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            openListTrackerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            openListTrackerButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -71),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            titleLabel.bottomAnchor.constraint(equalTo: openListTrackerButton.topAnchor, constant: -105)
         ])
+    }
+    
+    @objc private func openTrackers() {
+        let tabBarVC = TabBarController()
+        tabBarVC.modalPresentationStyle = .fullScreen
+        present(tabBarVC, animated: true)
     }
 }
