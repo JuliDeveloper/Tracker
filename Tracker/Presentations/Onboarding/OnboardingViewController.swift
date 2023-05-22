0279ed18844a1b0adadf531cf003ebb073c9b99c
JuliaRomanenko <rja-97@yandex.ru>
2023-05-22 17:43:33 +0300

    SPRINT-16: Поменяла анимацию перехода для UIPageViewController

--- Tracker/Presentations/Onboarding/OnboardingViewController.swift
+++ Tracker/Presentations/Onboarding/OnboardingViewController.swift
@@ -27,6 +27,18 @@ final class OnboardingViewController: UIPageViewController {
     }()
     
     //MARK: - Lifecycle
+    init() {
+        super.init(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal,
+            options: nil
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
